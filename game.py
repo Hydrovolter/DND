e4566d0da713a2a9f0d30663ea233f506f5ef151
Hydrovolter <103881740+Hydrovolter@users.noreply.github.com>
2023-05-09 20:51:11 +0100

    swap range len with enumerate for efficiency

--- game.py
+++ game.py
@@ -72,9 +72,13 @@ def add_xp(player_xp, xp_to_add):
 def get_level(player_xp):
     # DND XP System
     xp_thresholds = [0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000]
-    for i in range(len(xp_thresholds)):
-        if player_xp < xp_thresholds[i]:
-            return i-1
+    # for i in range(len(xp_thresholds)):
+    #     if player_xp < xp_thresholds[i]:
+    #         return i-1
+    # return 20
+    for i, threshold in enumerate(xp_thresholds):
+        if player_xp < threshold:
+            return i - 1
     return 20
 
 # Action Panel System
@@ -919,9 +923,14 @@ def startGame(player_name):
     # Third Event
     # thirdEvent()
 
+def debug():
+    pass
 
 
 
 # Start Game
 
 startGame(playerName)
+
+
+# debug()
